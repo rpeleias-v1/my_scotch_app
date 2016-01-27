fefc31e8e87315de5aefc17049e4a2b9b60e507f
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-01-27 19:57:25 -0200

    Fisrt commit

---
+++ app/routes/routes.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var User = require('../models/model.js');
+
+module.exports = function(app) {
+
+	app.get('/users', function(req, res) {
+		var query = User.find({});
+		query.exec(function(err, users) {
+			if (err) {
+				res.send(err);
+			}
+			res.json(users);
+		});
+	});
+
+	app.post('/users', function(req, res) {
+		var newUser = new User(req.body);
+
+		newUser.save(function(err) {
+			if(err) {
+				res.send(err);
+			} 
+			res.json(req.body);
+		});
+	});
+}
